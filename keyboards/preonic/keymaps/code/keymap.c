536eb3c62cbd76c9a5534cc011cd5ab3dfb9a09a
Vladislav Utkin <utkinvladislav@yandex.ru>
2023-06-30 22:47:27 +0300

    added copy/paste

--- keyboards/preonic/keymaps/code/keymap.c
+++ keyboards/preonic/keymaps/code/keymap.c
@@ -83,8 +83,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT_preonic_grid(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NUM , KC_PSLS, KC_PAST, KC_PMNS, _______,
         XXXXXXX, KC_F1  , KC_F2  , KC_F3  , KC_F4  , XXXXXXX, XXXXXXX, KC_P7  , KC_P8  , KC_P9  , KC_PPLS, KC_DEL ,
-        _______, KC_F5  , KC_F6  , KC_F7  , KC_F8  , XXXXXXX, XXXXXXX, KC_P4  , KC_P5  , KC_P6  , KC_PPLS, XXXXXXX,
-        _______, KC_F9  , KC_F10 , KC_F11 , KC_F12 , XXXXXXX, XXXXXXX, KC_P1  , KC_P2  , KC_P3  , KC_PENT, XXXXXXX,
+        _______, KC_F5  , KC_F6  , KC_F7  , KC_F8 ,C(KC_INS), XXXXXXX, KC_P4  , KC_P5  , KC_P6  , KC_PPLS, XXXXXXX,
+        _______, KC_F9  , KC_F10 , KC_F11 , KC_F12,S(KC_INS), XXXXXXX, KC_P1  , KC_P2  , KC_P3  , KC_PENT, XXXXXXX,
         _______, _______, _______, _______, _______, _______, _______, _______, KC_P0  , KC_PDOT, KC_PENT, XXXXXXX
         ),
 
@@ -145,9 +145,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_ADJUST] = LAYOUT_preonic_grid(
         KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
-        _______, QK_BOOT, DEBUG  , _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL ,
-        _______, _______, MU_MOD , AU_ON  , AU_OFF , _______, _______, DVORAK , QWERTY , _______, _______, _______,
-        _______, MUV_DE , MUV_IN , MU_ON  , MU_OFF , MI_ON  , MI_OFF , MOUSE  , KC_MPRV, KC_BRID, KC_BRIU, KC_MNXT,
+        _______, QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL ,
+        _______, _______, _______, AU_ON  , AU_OFF , _______, _______, DVORAK , QWERTY , _______, _______, _______,
+        _______, _______, _______, MU_ON  , MU_OFF , MI_ON  , MI_OFF , MOUSE  , KC_MPRV, KC_BRID, KC_BRIU, KC_MNXT,
         BACKLIT, RGB_TOG, _______, _______, _______, KC_CAPS, KC_CAPS, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY
         )};
 
